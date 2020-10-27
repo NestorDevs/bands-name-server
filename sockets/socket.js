03df23fde349a087db3ad4619422944fbbb2fb2f
NestorDevs <neivertjosias@gmail.com>
2020-10-27 15:17:55 -0400

    first commit

---
+++ sockets/socket.js
@@ -0,0 +1,44 @@
+const { io } = require("../index");
+const Band = require("../models/band");
+const Bands = require("../models/bands");
+
+const bands = new Bands();
+
+bands.addBand(new Band("Oasis"));
+bands.addBand(new Band("GnR"));
+bands.addBand(new Band("Nirvana"));
+bands.addBand(new Band("Zapato3"));
+
+// Mensajes del Sockets
+io.on("connection", (client) => {
+  console.log("Cliente conectado");
+
+  client.emit("active-bands", bands.getBands());
+
+  client.on("disconnect", () => {
+    console.log("Cliente desconectado");
+  });
+
+  client.on("mensaje", (payload) => {
+    console.log("Mensaje!!!", payload);
+
+    io.emit("mensaje", { admin: "NUevo mensaje" });
+  });
+
+  client.on("vote-band", (payload) => {
+    bands.voteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
+  });
+
+  client.on("add-band", (payload) => {
+    console.log(payload);
+    const newBand = new Band(payload.name);
+    bands.addBand(newBand);
+    io.emit("active-bands", bands.getBands());
+  });
+
+  client.on("delete-band", (payload) => {
+    bands.deleteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
+  });
+});
